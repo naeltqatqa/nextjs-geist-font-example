42c3a062d6138824588314f337cd296697bc042e
nael tqatqa <LONGHOP11@GMAIL.COM>
2025-07-20 16:52:42 +0300

    Add .next/static/chunks/src_app_about_page_tsx_a993527f._.js 1753019562555

---
+++ .next/static/chunks/src_app_about_page_tsx_a993527f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ff01fbe0._.js"
+],
+    source: "dynamic"
+});
